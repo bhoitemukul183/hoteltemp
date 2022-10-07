8222b0d88579867616c02d3514880fb28def4db2
bhoitemukul183 <bhoitemukul183@gmail.com>
2022-10-07 23:18:03 +0530

    Updates

---
+++ precache-manifest.cfda7693518d01cfe750852d3aa7a368.js
@@ -0,0 +1,274 @@
+self.__precacheManifest = [
+  {
+    "revision": "08820078a8a242b13ea1",
+    "url": "/hoteltemp/static/css/main.050ac5f2.chunk.css"
+  },
+  {
+    "revision": "08820078a8a242b13ea1",
+    "url": "/hoteltemp/static/js/main.08820078.chunk.js"
+  },
+  {
+    "revision": "d987a21e7126863f2f87",
+    "url": "/hoteltemp/static/js/1.d987a21e.chunk.js"
+  },
+  {
+    "revision": "176cfc666c18012a9809",
+    "url": "/hoteltemp/static/js/2.176cfc66.chunk.js"
+  },
+  {
+    "revision": "50367bdd5e397c3cbb97",
+    "url": "/hoteltemp/static/js/runtime~main.50367bdd.js"
+  },
+  {
+    "revision": "dbcaf98eb7a264d86182dca1ccc6e148",
+    "url": "/hoteltemp/static/media/m1.dbcaf98e.png"
+  },
+  {
+    "revision": "27d1e0d721193c918d45dbd0edc76b2d",
+    "url": "/hoteltemp/static/media/m3.27d1e0d7.png"
+  },
+  {
+    "revision": "6fc93a54bdf20247bfef6ada9babe44b",
+    "url": "/hoteltemp/static/media/t2.6fc93a54.png"
+  },
+  {
+    "revision": "aef439ff4a3435499339d9cf13134f70",
+    "url": "/hoteltemp/static/media/t3.aef439ff.png"
+  },
+  {
+    "revision": "c99cb2cc68bc7e1c8f9f1c97a8dabc34",
+    "url": "/hoteltemp/static/media/t4.c99cb2cc.png"
+  },
+  {
+    "revision": "704e3198d33f2eb3184dd3b826a87610",
+    "url": "/hoteltemp/static/media/pizza.704e3198.png"
+  },
+  {
+    "revision": "35d2fdc01244122bcce30abaf4cf13dd",
+    "url": "/hoteltemp/static/media/m5.35d2fdc0.png"
+  },
+  {
+    "revision": "71bdf2c77bf93db0dbbf12ed3b46a5c9",
+    "url": "/hoteltemp/static/media/t1.71bdf2c7.png"
+  },
+  {
+    "revision": "541c0a6a30999531d5740fa333a1597a",
+    "url": "/hoteltemp/static/media/m4.541c0a6a.png"
+  },
+  {
+    "revision": "15ac4983334c5042b1b62806573eedcc",
+    "url": "/hoteltemp/static/media/m6.15ac4983.png"
+  },
+  {
+    "revision": "2cd7b7bef3ba43450d67b9480cfa3355",
+    "url": "/hoteltemp/static/media/t5.2cd7b7be.png"
+  },
+  {
+    "revision": "1ee087ca187847726b9dea04e699c550",
+    "url": "/hoteltemp/static/media/bread.1ee087ca.jpg"
+  },
+  {
+    "revision": "aee2d4bbe6c70f0594a37f6408de7f22",
+    "url": "/hoteltemp/static/media/offer.aee2d4bb.png"
+  },
+  {
+    "revision": "96991b15b894f914387c32bedb22da85",
+    "url": "/hoteltemp/static/media/logo.96991b15.png"
+  },
+  {
+    "revision": "6c49862f840e4f2362f6a7ade6cfe76f",
+    "url": "/hoteltemp/static/media/pizza.6c49862f.jpg"
+  },
+  {
+    "revision": "e1323a7d0fcb1c7559e5e8a1a69c840b",
+    "url": "/hoteltemp/static/media/main.e1323a7d.jpg"
+  },
+  {
+    "revision": "1438f3488fd9510950cf049b7c73e178",
+    "url": "/hoteltemp/static/media/soup.1438f348.jpg"
+  },
+  {
+    "revision": "8b1e19efd21e93b207c0f4c2dc428ecd",
+    "url": "/hoteltemp/static/media/starter.8b1e19ef.jpg"
+  },
+  {
+    "revision": "eea7cbc1cae13a82cc22ae01bc0b81dc",
+    "url": "/hoteltemp/static/media/colddrink.eea7cbc1.jpg"
+  },
+  {
+    "revision": "b79e08fd6562385a05267d90337837fc",
+    "url": "/hoteltemp/static/media/mocktail.b79e08fd.jpg"
+  },
+  {
+    "revision": "7f2dd955b56e6ee54f5f69d1a9c8b63e",
+    "url": "/hoteltemp/static/media/rum.7f2dd955.jpg"
+  },
+  {
+    "revision": "21550feacfc43e960f670254523d93d9",
+    "url": "/hoteltemp/static/media/tea.21550fea.jpg"
+  },
+  {
+    "revision": "365faad0f8faca6bfb118ef61accc9ff",
+    "url": "/hoteltemp/static/media/vodka.365faad0.jpg"
+  },
+  {
+    "revision": "87aeb2e8eb42435315b51503a09d279d",
+    "url": "/hoteltemp/static/media/whisky.87aeb2e8.jpg"
+  },
+  {
+    "revision": "a54d206dd732d0a366ecc226ad5770da",
+    "url": "/hoteltemp/static/media/1.a54d206d.jpg"
+  },
+  {
+    "revision": "96f4eb6e96b34ac55597a8638796a4a3",
+    "url": "/hoteltemp/static/media/2.96f4eb6e.jpg"
+  },
+  {
+    "revision": "7d96d9dc7fa81da86aa0e58bfbfc5a89",
+    "url": "/hoteltemp/static/media/3.7d96d9dc.jpg"
+  },
+  {
+    "revision": "d8abeab64f5fe9f07989237575281be0",
+    "url": "/hoteltemp/static/media/4.d8abeab6.jpg"
+  },
+  {
+    "revision": "826e656cb246e43d1d3ef94d4e0c94f5",
+    "url": "/hoteltemp/static/media/5.826e656c.jpg"
+  },
+  {
+    "revision": "b755da58455306d4f192e80e05867242",
+    "url": "/hoteltemp/static/media/6.b755da58.jpg"
+  },
+  {
+    "revision": "620e3b2d96eaea4164bac0b89ca7aae4",
+    "url": "/hoteltemp/static/media/7.620e3b2d.png"
+  },
+  {
+    "revision": "e9c1e7768726c93db0067f1d20a56026",
+    "url": "/hoteltemp/static/media/8.e9c1e776.png"
+  },
+  {
+    "revision": "eb7d7e326bc49280cc310be271eec66c",
+    "url": "/hoteltemp/static/media/9.eb7d7e32.png"
+  },
+  {
+    "revision": "870b66f53db03b6b1923900bb4f57bb4",
+    "url": "/hoteltemp/static/media/1.870b66f5.jpg"
+  },
+  {
+    "revision": "a4a17557bafd5a1b1ed2eb1a1754ee80",
+    "url": "/hoteltemp/static/media/2.a4a17557.jpeg"
+  },
+  {
+    "revision": "4a9abbf3ae84ed0f049c241b6de7ee9c",
+    "url": "/hoteltemp/static/media/3.4a9abbf3.jpg"
+  },
+  {
+    "revision": "60c775ee600d2b714000e271ab1a419a",
+    "url": "/hoteltemp/static/media/4.60c775ee.jpg"
+  },
+  {
+    "revision": "f92bf09681863359f0a1c39b56084905",
+    "url": "/hoteltemp/static/media/5.f92bf096.jpg"
+  },
+  {
+    "revision": "a90facd61ea62874c78a7ff7a3f865fc",
+    "url": "/hoteltemp/static/media/6.a90facd6.jpg"
+  },
+  {
+    "revision": "fa527f42f5dbae0455838de8ce53ce22",
+    "url": "/hoteltemp/static/media/7.fa527f42.jpeg"
+  },
+  {
+    "revision": "c667fd56964c246cbfc628eecea01b30",
+    "url": "/hoteltemp/static/media/8.c667fd56.jpg"
+  },
+  {
+    "revision": "4123b389141c678f2e4f3e0fd763cb0a",
+    "url": "/hoteltemp/static/media/9.4123b389.jpeg"
+  },
+  {
+    "revision": "202929b3bd99829b75afb3e7158f508a",
+    "url": "/hoteltemp/static/media/banner1.202929b3.jpg"
+  },
+  {
+    "revision": "475c5a095a6661e7b0a5b0a5ad86e8a4",
+    "url": "/hoteltemp/static/media/banner2.475c5a09.png"
+  },
+  {
+    "revision": "cf1764d0c4df452b13ecd5625decc047",
+    "url": "/hoteltemp/static/media/banner3.cf1764d0.jpg"
+  },
+  {
+    "revision": "8935c33b77ca78e8c99fb13835381791",
+    "url": "/hoteltemp/static/media/banner4.8935c33b.jpg"
+  },
+  {
+    "revision": "2214384fc7f3a5d97fe728ae7b336717",
+    "url": "/hoteltemp/static/media/banner5.2214384f.jpg"
+  },
+  {
+    "revision": "12f2b7c33738a7f8d05b694265fa3e88",
+    "url": "/hoteltemp/static/media/offer1.12f2b7c3.png"
+  },
+  {
+    "revision": "fce8fe6485958e651bc25e193b4a400a",
+    "url": "/hoteltemp/static/media/offer2.fce8fe64.png"
+  },
+  {
+    "revision": "38020b1af66176967fcac354419e86fd",
+    "url": "/hoteltemp/static/media/offer3.38020b1a.png"
+  },
+  {
+    "revision": "8d700f527fad79ae17c959492d669a9d",
+    "url": "/hoteltemp/static/media/c1.8d700f52.png"
+  },
+  {
+    "revision": "3554269ad4b6c77e176b0d4c1722a4f1",
+    "url": "/hoteltemp/static/media/c2.3554269a.png"
+  },
+  {
+    "revision": "60d2f3d9fce56e9b6065c0e90a335b01",
+    "url": "/hoteltemp/static/media/c3.60d2f3d9.png"
+  },
+  {
+    "revision": "ab2212495215e621c2efb03b5de65a92",
+    "url": "/hoteltemp/static/media/c4.ab221249.png"
+  },
+  {
+    "revision": "27eea2e6de463f681f48f1734eb71542",
+    "url": "/hoteltemp/static/media/c5.27eea2e6.png"
+  },
+  {
+    "revision": "7d2e09319debf1f3a94c7b27a75af0c6",
+    "url": "/hoteltemp/static/media/1.7d2e0931.jpg"
+  },
+  {
+    "revision": "ef41d9d9786b05193fe8f8784c9369aa",
+    "url": "/hoteltemp/static/media/2.ef41d9d9.jpg"
+  },
+  {
+    "revision": "40415688df31119a2edac6a95eddcb73",
+    "url": "/hoteltemp/static/media/3.40415688.jpg"
+  },
+  {
+    "revision": "8faf369a70377fc2524016c2d94d6b4c",
+    "url": "/hoteltemp/static/media/4.8faf369a.jpg"
+  },
+  {
+    "revision": "f182be589f2b2787929b159dc0d03eae",
+    "url": "/hoteltemp/static/media/5.f182be58.jpg"
+  },
+  {
+    "revision": "7223bf97381b1ce3821fa8f6e5a22388",
+    "url": "/hoteltemp/static/media/6.7223bf97.jpg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/hoteltemp/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "e378ff522de89570ef6fce939d942cf2",
+    "url": "/hoteltemp/index.html"
+  }
+];
